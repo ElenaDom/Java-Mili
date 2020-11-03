e0b31f91f1c36508976df9cf8192ebe8dba71cd7
Elena Domnina <bal.atidom@gmail.com>
2020-11-03 15:22:21 +0400

    Second

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int amount = 9830;
         int percent = 20;
-                int bonus = amount / percent;
+        int bonus = amount / percent;
         System.out.println(bonus);
     }
     }
