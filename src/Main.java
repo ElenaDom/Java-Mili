617f370adc490bdf009e162cd785f9ac4670876b
Elena Domnina <bal.atidom@gmail.com>
2020-11-03 13:20:25 +0400

    First

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 9830;
+        int percent = 20;
+                int bonus = amount / percent;
+        System.out.println(bonus);
+    }
+    }
+
